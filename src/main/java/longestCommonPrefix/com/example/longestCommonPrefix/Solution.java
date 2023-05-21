97f2fcc91a1caad2783836505b16a08caeaa4998
earl-stephens <stephensearl@hotmail.com>
2023-05-20 21:42:07 -0600

    Test and method for longestCommonPrefix

--- src/main/java/longestCommonPrefix/com/example/longestCommonPrefix/Solution.java
+++ src/main/java/longestCommonPrefix/com/example/longestCommonPrefix/Solution.java
@@ -1,11 +1,26 @@
 package longestCommonPrefix.com.example.longestCommonPrefix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
 
 	public String longestCommonPrefix(String[] inputString) {
-		for(String word: inputString) {
-			System.out.println(word.valueOf(word.toCharArray(), 0, 2));
+		Arrays.sort(inputString, (string1, string2) -> string1.length() - string2.length());
+		int shortestWord = inputString[0].length();
+		String outputString = "";
+		
+		List<String> intermediateArray = new ArrayList<>();
+		for(int i = 0; i < shortestWord; i++) {
+			for(String word: inputString) {				
+				intermediateArray.add(word.valueOf(word.toCharArray(), 0, (i + 1)));
+			}
+			if(intermediateArray.stream().distinct().count() == 1) {
+				outputString = intermediateArray.get(0);
+				intermediateArray.clear();
+			}
 		}
-		return "";
+		return outputString;
 	}
 }
